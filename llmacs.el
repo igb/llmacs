a4d9cf164fb2656d65f042275b8e4f25134d4da7
Ian Brown <ibrown@netflix.com>
2025-03-13 19:55:27 -0700

    getting selected text

--- llmacs.el
+++ llmacs.el
@@ -2,4 +2,15 @@
   "Interact with ChattGPT"
   
   (interactive)
-  )
+
+
+  (if mark-active
+      (let (
+	    (selection (buffer-substring-no-properties (region-beginning) (region-end))))
+	(if (= (length selection) 0)
+	    (print "nothing selected")
+	  (print selection))
+
+
+
+  )))
